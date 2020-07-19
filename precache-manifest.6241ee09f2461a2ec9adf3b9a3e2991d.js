6e204f9aaf8f0ab8e5d9755d81b992c330e93d9a
Joanna Daszkiewicz <asia.daszkiewicz@wp.pl>
2020-07-19 14:48:02 +0200

    Updates

---
+++ precache-manifest.6241ee09f2461a2ec9adf3b9a3e2991d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d201190be85502f8528d0f1e0d26ef73",
+    "url": "/movieapp/index.html"
+  },
+  {
+    "revision": "1601de83df422f2c418c",
+    "url": "/movieapp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fb6447f7b2896010c16b",
+    "url": "/movieapp/static/css/main.e1449dde.chunk.css"
+  },
+  {
+    "revision": "1601de83df422f2c418c",
+    "url": "/movieapp/static/js/2.a54a1fe4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/movieapp/static/js/2.a54a1fe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb6447f7b2896010c16b",
+    "url": "/movieapp/static/js/main.94abaab1.chunk.js"
+  },
+  {
+    "revision": "0d516f28c241e3c70098",
+    "url": "/movieapp/static/js/runtime-main.04b21085.js"
+  }
+]);
